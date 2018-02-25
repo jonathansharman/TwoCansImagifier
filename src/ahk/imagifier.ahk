03d723806c9314c5a25a552cb5b7bb9e00c9ab71
Jonathan Sharman <jonathan.sharman@gmail.com>
2018-02-24 19:03:52 -0600

    Initial commit.

---
+++ src/ahk/imagifier.ahk
@@ -0,0 +1,80 @@
+﻿#CommentFlag //
+SendMode input
+
+// Include definition for image array.
+#Include data.ahk
+
+// Sets the current draw color to the given RGB array value. Color elements are in [0, 255].
+set_color(rgb) {
+	global origin_x
+	global origin_y
+
+	static top_offset := 1
+	static bottom_offset := 320
+	static rgb_offsets := [415, 475, 535]
+
+	Loop, 3 {
+		click_x := origin_x + rgb_offsets[A_index]
+		click_y := origin_y + top_offset + (bottom_offset - top_offset) * rgb[A_index] / 255
+		Click, %click_x%, %click_y%
+	}
+}
+
+// Sets the pixel at the given 0-based image coordinates to the current color.
+set_pixel(x, y) {
+	global origin_x
+	global origin_y
+
+	static pixel_size := 10
+	static pixel_offset := 4
+
+	click_x := origin_x + x * pixel_size + pixel_offset
+	click_y := origin_y + y * pixel_size + pixel_offset
+	Click, %click_x%, %click_y%
+}
+
+// Converts an RGB AutoHotkey color to an RGB array.
+rgb_color_to_rgb_array(color) {
+	return [((color & 0xFF0000) >> 16), ((color & 0xFF00) >> 8), (color & 0xFF)]
+}
+
+// Get the origin coordinates.
+MsgBox Click to the left of the image border.
+KeyWait LButton, D
+MouseGetPos origin_x, origin_y
+while (color != 0) {
+	// Move right and recheck.
+	origin_x++
+	PixelGetColor color, %origin_x%, %origin_y%
+	MouseMove, %origin_x%, %origin_y%
+}
+PixelGetColor color, %origin_x%, %origin_y%
+while (color == 0) {
+	// Move up and recheck.
+	origin_y--
+	PixelGetColor color, %origin_x%, %origin_y%
+	MouseMove, %origin_x%, %origin_y%
+}
+origin_y++
+MouseMove, %origin_x%, %origin_y%
+
+// Loop over image.
+x := 0
+y := 0
+image_res = 32
+loop_count := image_res * image_res
+Loop, %loop_count% {
+	// Set color.
+	set_color(image[A_index])
+
+	// Draw.
+	set_pixel(x, y)
+
+	x++
+	if (x == IMAGE_RES) {
+		x := 0
+		y++
+	}
+}
+// Exit.
+ExitApp
---
+++ src/c#/TwoCansImageConverter/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TwoCansImageConverter
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ImportButton_Click(object sender, RoutedEventArgs e)
+		{
+			const int redOffset = 2;
+			const int greenOffset = 1;
+			const int blueOffset = 0;
+
+			// Get image filename.
+			Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+			dlg.Title = "Import Profile Image";
+			if (dlg.ShowDialog() == true) {
+				// Open image as bitmap.
+				string filename = dlg.FileName;
+				BitmapImage image = new BitmapImage(new Uri(filename));
+
+				// Get image properties.
+				int width = image.PixelWidth;
+				int height = image.PixelHeight;
+				int bytesPerPixel = (image.Format.BitsPerPixel + 7) / 8;
+				int stride = width * bytesPerPixel;
+
+				// Convert image to byte array.
+				byte[] imageData = new byte[height * stride];
+				image.CopyPixels(imageData, stride, 0);
+
+				// Build image string.
+				StringBuilder imageString = new StringBuilder("image := []\n");
+				int pixelCount = imageData.Length / bytesPerPixel;
+				for (int pixelIdx = 0; pixelIdx < pixelCount; ++pixelIdx) {
+					int byteIdx = pixelIdx * bytesPerPixel;
+					imageString.Append("image.Push(");
+					imageString.Append("[" + imageData[byteIdx + redOffset]);
+					imageString.Append("," + imageData[byteIdx + greenOffset]);
+					imageString.Append("," + imageData[byteIdx + blueOffset] + "])\n");
+				}
+
+				// Set textbox text and copy to clipboard.
+				TextBox.Text = imageString.ToString();
+				TextBox.SelectAll();
+				Clipboard.SetText(TextBox.Text);
+				CopiedLabel.Visibility = System.Windows.Visibility.Visible;
+
+				// Hide label after 2 seconds.
+				DispatcherTimer timer = new DispatcherTimer();
+				timer.Interval = TimeSpan.FromSeconds(2.0);
+				timer.Tick += new EventHandler(delegate(object s, EventArgs a) {
+					CopiedLabel.Visibility = System.Windows.Visibility.Hidden;
+				});
+				timer.Start();
+			}
+		}
+
+		private void ExportButton_Click(object sender, RoutedEventArgs e)
+		{
+			Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+			dlg.Title = "Export Image Data";
+			dlg.FileName = "data.ahk";
+			dlg.Filter = "AutoHotkey Script|*.ahk";
+			if (dlg.ShowDialog() == true) {
+				File.WriteAllText(dlg.FileName, TextBox.Text);
+			}
+		}
+	}
+}
